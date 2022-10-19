49066001428c67ca398f74db674fde00c8cab3b5
Rob <robrichardsdpt@gmail.com>
2022-10-19 19:22:08 -0400

    updated input component

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,5 +1,14 @@
 import React from "react";
+import "./Input.styles.scss";
 
-const Input = () => <input />;
+const Input = ({
+  altText,
+  classNameArray,
+  onChange,
+}: {
+  altText?: string;
+  classNameArray?: string;
+  onChange?: () => void;
+}) => <input alt={altText} className={classNameArray} />;
 
 export default Input;
