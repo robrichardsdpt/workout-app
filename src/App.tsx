38924b030724572d72951e2b34a338e167c7cf89
Rob <robrichardsdpt@gmail.com>
2022-10-19 21:09:40 -0400

    added Button styles and component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.css";
 import Input from "./components/Input/Input";
+import Button from "./components/Button/Button";
 
 function App() {
   const handleChange = (event: { target: HTMLInputElement }) =>
@@ -11,6 +12,8 @@ function App() {
       <h1>Calculate your Percentage based off of 1RM</h1>
       <Input required={true} inputName="percentage" onChange={handleChange} />
       <Input required={true} inputName="weight" onChange={handleChange} />
+      <Button className="primary">Button</Button>
+      <Button className="secondary">Button</Button>
     </div>
   );
 }
